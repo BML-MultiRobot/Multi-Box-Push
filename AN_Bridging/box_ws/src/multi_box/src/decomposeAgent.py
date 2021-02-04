b9c1261f8cb03e872bf474e94f1977a8529207de
Austin Nguyen <austinnguyen517@berkeley.edu>
2021-02-03 20:23:25 -0800

    finished

--- AN_Bridging/box_ws/src/multi_box/src/decomposeAgent.py
+++ AN_Bridging/box_ws/src/multi_box/src/decomposeAgent.py
@@ -17,11 +17,6 @@ from Algs.hierarchical_MBRL import Hierarchical_MBRL
 NAME = 'bot'
 
 algs = {
-    0: 'INVERSE',
-    1: 'CONTROL',
-    2: 'DOUBLE_CONTROL',
-    3: 'OMNI_CONTROL',
-    4: 'PLANNER',
     5: 'MBRL',
     6: 'STIGMERGIC',
     7: 'TEST'
@@ -29,92 +24,6 @@ algs = {
 ALGORITHM = 5
 description = algs[ALGORITHM]
 rospy.init_node('Dummy', anonymous=True)
-
-if description == 'INVERSE':
-    agents = OrderedDict({
-                #ensure ordering matches ros messages
-                "bot":          {"sub": "/state", "pub": "/action"} #joint action space
-            })
-    
-    params = {
-            'clusters':     3,
-            'mode':         'RNN', #Spectral, RNN
-            'state_n':      4, # this does not include time
-            'horizon':      6,
-            'noise':        False
-        }
-    params = {"params": params, "agents": agents}
-
-    bot = Decompose(params, NAME, DecomposeTask())
-
-if description == 'OMNI_CONTROL':
-    NAME = 'a_bot'
-    agents = OrderedDict({
-                #ensure ordering matches ros message
-                "a_bot": {"sub": "/state", "pub": "/action1"}, #joint action space
-            })
-    agents["b_bot"] = {"sub": "/state", "pub": "/action2"}
-    agents["c_bot"] = {"sub": "/state", "pub": "/action3"}
-    #agents["d_bot"] = {"sub": "/state", "pub": "/action4"}
-
-    # Placeholders
-    valPars = {
-                'neurons':      (18, 256, 256, 8),
-                'act':          ['F.leaky_relu','F.leaky_relu'],
-                'mu':           torch.Tensor([-2, 0, 5, 0, 0, 0, 
-                                              -1, 0, .5,0, 0, 0,
-                                              -1, 0, .5,0, 0, 0]),
-                'std':          torch.Tensor([1, 1, 1, np.pi, np.pi, np.pi,
-                                              1, 1, 1, np.pi, np.pi, np.pi,
-                                              1, 1, 1, np.pi, np.pi, np.pi]),
-                'trainMode':    True,
-                'load':         False, 
-                'dual':         False,
-                }             
-    valTrain = {
-                'batch':        128, 
-                'lr':           3e-4, 
-                'buffer':       500,
-                'gamma':        .99,
-                'explore':      .9, 
-                'double':       True,
-                }
-    params = {"valPars": valPars, "valTrain": valTrain, "agents": agents}
-    tanker = DoubleQ(params, NAME, OmniBoxSlopeTask())
-
-if description == 'PLANNER':
-    NAME = 'a_bot'
-    agents = OrderedDict({
-                #ensure ordering matches ros message
-                "a_bot": {"sub": "/state", "pub": "/action1"}, #joint action space
-            })
-    agents["b_bot"] = {"sub": "/state", "pub": "/action2"}
-    # agents["c_bot"] = {"sub": "/state", "pub": "/action3"}
-
-    policyPars = {
-                'neurons':      (10, 256, 256, 8), # 5 box-related state, 4 goal-related state, 1 indicator, 8 controls
-                'act':          ['F.leaky_relu', 'F.leaky_relu'],
-                'mu':           torch.Tensor([0, 0, 0, 0, 0,
-                                              0, 0, 0, 0, 0]),
-                'std':          torch.Tensor([1, 1, 1, 1, 1,
-                                              1, 1, 1, 1, 1]),
-                'trainMode':    False,  # Make sure both value and policy are set to the same thing
-                'load':         False, 
-                'dual':         False,
-                } 
-    policyTrain = {
-                'batch':        256, 
-                'lr':           3e-4, 
-                'buffer':       5000,
-                'gamma':        .95, 
-                'explore':      0,  # Don't change this. Tune the exploration up top
-                'double':       True,
-                'noise':        0,
-                'priority':     True
-                }
-    
-    params = {"valPars": policyPars, "valTrain": policyTrain, "agents": agents}
-    planner = Planner(params, NAME)
 
 if description == 'MBRL':
     NAME = 'a_bot'
